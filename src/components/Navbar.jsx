a4e806d742b7fb17a0273e42be50f293fba2ac2a
Param <parampatel1025@gmail.com>
2025-11-30 13:51:07 -0700

    Resume Upload and Download Functionality done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import { navIcons, navLinks } from '#constants/Index'
 import React from 'react'
 import dayjs from 'dayjs'
+import useWindowStore from '#store/window'
 
 
 const Navbar = () => {
+
+    const {openWindow} = useWindowStore();
+    
+
   return (
     <nav>
         <div>
             <img src='/images/logo.svg' alt='logo'/>
             <p className='font-bold'>Param's Portfolio</p>
             <ul>
-                {navLinks.map(({ id,name })=>(
-                    <li key={id}>
+                {navLinks.map(({ id, name, type }) => (
+                    <li key={id} onClick={() => openWindow(type)}>
                         <p>{name}</p>
                     </li>
                 ))}
